898b004b91026846ec0ddc60558fb961ba25c51f
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-08-19 22:06:05 +0900

    practice

---
+++ 2.py
@@ -0,0 +1,16 @@
+favorite_fruit = ['apple', 'orange', 'banaan']
+print(favorite_fruit)
+ 
+# リストの１つめ
+print(favorite_fruit[0])
+# リストの2つめ
+print(favorite_fruit[1])
+# リストの3つめ
+print(favorite_fruit[2])
+
+apple_price = 150
+ 
+if apple_price < 200:
+    print("割引価格です。")
+else:
+    print("通常価格です。")
